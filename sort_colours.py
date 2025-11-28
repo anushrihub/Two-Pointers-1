c1649ba2774fd7b7022a04638b3742ee76931af2
Anushri <sastikaranushri@gmail.com>
2025-11-28 15:03:42 -0500

    optmised solution

--- sort_colours.py
+++ sort_colours.py
@@ -1,18 +1,35 @@
 # https://leetcode.com/problems/sort-colors/
 
-# brutforce Tc- O(n log n) Sc- (1)
-# class Solution:
-#     def sortColors(self,nums: list[int]) -> list[int]:
-#         nums.sort()
-#         return nums
-
-# brutforce Tc- (n2) Sc-(1)
+# This algorithm sorts an array of 0s, 1s, and 2s using three pointers. It moves 0s to the left and 2s to the right by swapping, and 1 stay in the middle. When a 2 is swapped in, the index needs to recheck to ensure the new value is processed.
+# Time Complexity- O(n) Space Complexity- O(1)
 class Solution:
-    def sortColors(self, nums: list[int]) -> list[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range(0, n - i - 1):
-                if nums[j] > nums[j + 1]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
-        return nums
+    def sortColors(self, nums: list[int]) -> None:
+        l, r = 0, len(nums) - 1
+        # curent index
+        i = 0
+        # swapping the elements
+        def swap(i, j):
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
 
+        while i <= r:
+            # when the current element is 0, it should be on left side
+            if nums[i] == 0:
+                # swap it by calling the function
+                swap(l, i)
+                # increase the left pointer
+                l += 1
+            # when the current element is 2, it should be on right side
+            elif nums[i] == 2:
+                # swap it by calling the function
+                swap(i, r)
+                # decrease the right pointer
+                r -= 1
+                # after swapping, the right element comes to the begining so rechek that element
+                i -= 1
+            # increase the current index
+            i += 1
+
+solution = Solution()
+print(solution.sortColors([2,0,2,1,1,0]))
